40b0c697a9610bd4eefbba47cdbce865872c3309
joy <1686452422@qq.com>
2025-03-24 21:22:23 +0800

    add:柜机类型增删改查接口

--- share-modules/share-device/src/main/java/com/share/sharedevice/comtroller/CabinetTypeController.java
+++ share-modules/share-device/src/main/java/com/share/sharedevice/comtroller/CabinetTypeController.java
@@ -1,6 +1,7 @@
 package com.share.sharedevice.comtroller;
 
 import com.share.common.core.web.controller.BaseController;
+import com.share.common.core.web.domain.AjaxResult;
 import com.share.common.core.web.page.TableDataInfo;
 import com.share.sharedevice.entity.CabinetType;
 import com.share.sharedevice.service.CabinetTypeService;
@@ -8,11 +9,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -31,6 +30,42 @@ public class CabinetTypeController extends BaseController {
         startPage();
         List<CabinetType> list = cabinetTypeService.selectCabinetTypeList(cabinetType);
         return getDataTable(list);
+    }
+
+    @Operation(summary = "根据id查询详情")
+    @GetMapping("/{id}")
+    public AjaxResult getCabinetTypeById(@PathVariable("id") Long id) {
+        return success(cabinetTypeService.getById(id));
+    }
+
+    @Operation(summary = "添加柜机类型")
+    @PostMapping
+    public AjaxResult add(@RequestBody CabinetType cabinetType) {
+        boolean save = cabinetTypeService.save(cabinetType);
+        return toAjax(save);
+    }
+
+    @Operation(summary = "修改柜机类型")
+    @PutMapping
+    public AjaxResult update(@RequestBody CabinetType cabinetType) {
+        boolean update = cabinetTypeService.updateById(cabinetType);
+        return toAjax(update);
+    }
+
+    @Operation(summary = "删除柜机类型")
+    @DeleteMapping("/{ids}")
+    public AjaxResult deleteBatchList(@PathVariable Long[] ids) {
+        if (ids != null && ids.length > 0) {
+            boolean result = cabinetTypeService.removeBatchByIds(Arrays.asList(ids));
+            return toAjax(result);
+        }
+        return toAjax(true);
+    }
+
+    @Operation(summary = "查询全部柜机类型列表")
+    @GetMapping("/getCabinetTypeList")
+    public AjaxResult getCabinetTypeList() {
+        return success(cabinetTypeService.list());
     }
 
 
